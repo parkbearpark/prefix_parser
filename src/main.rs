171518bc8f5bbfde91cfccaea76fe50c6aedea27
yuma <yumai6205@gmail.com>
2020-08-02 19:43:20 +0900

    promptを作ってmainで実行できるようにする

--- src/main.rs
+++ src/main.rs
@@ -151,6 +151,34 @@ fn consume_byte(input: &[u8], pos: usize, b: u8) -> Result<(u8, usize), LexError
     Ok((b, pos + 1))
 }
 
+use std::io;
+
+fn prompt(s: &str) -> io::Result<()> {
+    use std::io::{stdout, Write};
+    let stdout = stdout();
+    let mut stdout = stdout.lock();
+    stdout.write(s.as_bytes())?;
+    stdout.flush()
+}
+
 fn main() {
-    println!("Hello, world!");
+    use std::io::{stdin, BufRead, BufReader};
+
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let stdin = BufReader::new(stdin);
+    let mut lines = stdin.lines();
+
+    loop {
+        prompt("> ").unwrap();
+
+        if let Some(Ok(line)) = lines.next() {
+            let token = lex(&line);
+            println!("{:?}", token);
+        } else {
+            break;
+        }
+    }
+}
+
 }
