e18c46c65837c645ba69938a2df3d50f03eb086a
yuma <yumai6205@gmail.com>
2020-08-02 18:52:48 +0900

    location

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Loc(usize, usize);
+
+impl Loc {
+    fn merge(&self, other: &Loc) -> Loc {
+        use std::cmp::{max, min};
+        Loc(min(self.0, other.0), max(self.1, other.1))
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
